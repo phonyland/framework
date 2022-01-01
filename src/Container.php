db9db1aadf951be1f9a90e4318d91493c409f787
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-01 16:02:26 +0300

    Container: It gets an instance

--- src/Container.php
+++ src/Container.php
@@ -3,9 +3,41 @@
 declare(strict_types=1);
 
 namespace Phonyland\Framework;
+
 /**
  * @internal
  */
 final class Container
 {
+    /**
+     * @var array<string, mixed>
+     */
+    private array $instances = [];
+
+    /**
+     * Adds the given instance to the container.
+     *
+     * @param  string  $id
+     * @param  mixed   $instance
+     */
+    public function add(string $id, mixed $instance): void
+    {
+        $this->instances[$id] = $instance;
+    }
+
+    /**
+     * Gets a dependency from the container.
+     *
+     * @param class-string $id
+     *
+     * @return mixed
+     */
+    public function get(string $id): mixed
+    {
+        if (! array_key_exists($id, $this->instances)) {
+            $this->instances[$id] = $this->build($id);
+        }
+
+        return $this->instances[$id];
+    }
 }
--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 use Phonyland\Framework\Container;
 
+beforeEach(function () {
+    $this->container = new Container();
+});
+
 it('exists')->assertTrue(class_exists(Container::class));
+
+it('gets an instance', function () {
+    // Arrange
+    $this->container->add(Container::class, $this->container);
+
+    // Act
+    expect($this->container->get(Container::class))->toBe($this->container);
+});
