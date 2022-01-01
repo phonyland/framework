8f430237ab16f766983070af06eeb37a3c61a069
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-01 17:00:17 +0300

    Add `ShouldNotHappen` exception

---
+++ src/Exceptions/ShouldNotHappen.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phonyland\Framework\Exceptions;
+
+use Exception;
+use RuntimeException;
+
+/**
+ * @internal
+ */
+final class ShouldNotHappen extends RuntimeException
+{
+    /**
+     * Creates a new Exception instance.
+     */
+    public function __construct(Exception $exception)
+    {
+        $message = $exception->getMessage();
+
+        parent::__construct(sprintf(<<<MESSAGE
+                
+                This should not happen - please create an new issue here: https://github.com/phonyland/framework.
+                
+                - Issue: %s
+                - PHP version: %s
+                - Operating system: %s
+                MESSAGE
+            , $message, PHP_VERSION, PHP_OS), 1, $exception);
+    }
+
+    /**
+     * Creates a new instance of should not happen without a specific exception.
+     */
+    public static function fromMessage(string $message): ShouldNotHappen
+    {
+        return new ShouldNotHappen(new Exception($message));
+    }
+}
