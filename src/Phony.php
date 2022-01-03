00749e472bb8f56513f5efd6f408311fe95b033e
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-04 01:15:59 +0300

    Phony: WIP magic setup

--- src/Phony.php
+++ src/Phony.php
@@ -6,4 +6,18 @@ namespace Phonyland\Framework;
 
 class Phony
 {
+    public function __get(string $name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function __set(string $name, $value): void
+    {
+        // TODO: Implement __set() method.
+    }
+
+    public function __isset(string $name): bool
+    {
+        // TODO: Implement __isset() method.
+    }
 }
