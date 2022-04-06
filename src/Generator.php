ceb02977b657baa4e30fe5093fd1b7cc065c02ec
Yunus Emre Deligöz <ye@deligoz.me>
2022-04-07 01:13:32 +0300

    Generator: Add `dataPackages` attribute

--- src/Generator.php
+++ src/Generator.php
@@ -6,6 +6,13 @@ namespace Phonyland\Framework;
 
 abstract class Generator
 {
+    /**
+     * Holds the list of data packages for the generator.
+     *
+     * @var array<string, string>
+     */
+    protected array $dataPackages = [];
+
     public function __construct(
         protected Phony $phony
     ) {
