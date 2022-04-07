401455947aa93597db66a9453ca3cbe392699f33
Yunus Emre Deligöz <ye@deligoz.me>
2022-04-07 03:36:36 +0300

    Pest: Add `dd()` and `d()` functions

--- tests/Pest.php
+++ tests/Pest.php
@@ -8,3 +8,20 @@ function 🙃(): Phony
 {
     return new Phony();
 }
+
+if (!function_exists('dd')) {
+    function dd()
+    {
+        $args = func_get_args();
+        call_user_func_array('dump', $args);
+        die();
+    }
+}
+
+if (!function_exists('d')) {
+    function d()
+    {
+        $args = func_get_args();
+        call_user_func_array('dump', $args);
+    }
+}
